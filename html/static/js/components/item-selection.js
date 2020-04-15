ba4ae1f6cd3da885f7e0812f5f9821d2ac6b421a
Cadence Ember <cloudrac3r@vivaldi.net>
2020-04-16 02:01:20 +1200

    Sort the item selection better

--- html/static/js/components/item-selection.js
+++ html/static/js/components/item-selection.js
@@ -15,11 +15,27 @@ class ItemList extends ElemJS {
 
 	render() {
 		this.clearChildren()
-		const filter = store.get("filter")
-		for (const item of store.items.values()) {
-			if (item.name.toLowerCase().includes(filter.toLowerCase())) {
-				this.child(new Item(item))
+		const filter = store.get("filter").toLowerCase()
+		/** @param {import("../../../../types").Item} item */
+		function calculateItemOrderAsString(item) {
+			const result = []
+			if (item.name.toLowerCase().startsWith(filter)) { // starting match bonus
+				result.push(0)
+			} else {
+				result.push(1)
 			}
+			result.push(item.name)
+			return result.join("\0")
+		}
+		const items = [...store.items.values()].filter(item => item.name.toLowerCase().includes(filter)).sort((a, b) => {
+			const a2 = calculateItemOrderAsString(a)
+			const b2 = calculateItemOrderAsString(b)
+			if (a2 < b2) return -1
+			else if (a2 === b2) return 0
+			else return 1
+		})
+		for (const item of items) {
+			this.child(new Item(item))
 		}
 		if (store.defaultTag) {
 			this.style("borderColor", store.defaultTag.getColor())
