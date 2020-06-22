93433fa02091994cfd2f031a59a3df849ca1ed7c
Cadence Ember <cloudrac3r@vivaldi.net>
2020-06-22 23:25:46 +1200

    Add clear list confirmation

--- html/static/js/components/clear-list.js
+++ html/static/js/components/clear-list.js
@@ -1,15 +1,23 @@
 import {store} from "../structures/Store.js"
 import {ElemJS, q} from "../elemjs/elemjs.js"
+import {ContextMenu} from "./context-menu.js"
 
 class ClearList extends ElemJS {
 	constructor() {
 		super(q("#clear-list"))
 
-		this.event("click", this.onClick.bind(this))
+		this.event("click", this.onRightClick.bind(this))
 	}
 
-	onClick() {
-		store.list.clear()
+	onRightClick(event) {
+		event.preventDefault()
+		new ContextMenu([
+			{text: "Really clear the list?\nThere's no going back.", color: "#bd1919", type: "label"},
+			{text: "Yeah do it", type: "button", fn: menu => {
+				store.list.clear()
+				menu.close()
+			}}
+		])
 	}
 }
 
