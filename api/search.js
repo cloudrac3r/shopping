a78ef12594c1bd1f98694fc2270d37f277a26c95
Cadence Ember <cadence@disroot.org>
2021-11-02 18:14:05 +1300

    Fix store search

--- api/search.js
+++ api/search.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch").default
+const entities = require("entities")
 const {parse} = require("fast-html-parser")
 
 class FrequencyMap {
@@ -29,7 +30,7 @@ class FrequencyMap {
 	split(text) {
 		const splitters = [" ", "-"]
 		const result = splitters.reduce((a, c) => a.flat(Infinity).map(x => x.split(c)), [text]).flat(Infinity)
-		console.log(text, "->", result)
+		// console.log(text, "->", result)
 		return result
 	}
 
@@ -58,24 +59,33 @@ module.exports = [
 			}
 
 			const query = url.searchParams.get("query")
-			const remoteURL = new URL("https://www.ishopnewworld.co.nz/Search")
+			const remoteURL = new URL("https://www.newworld.co.nz/shop/Search")
 			remoteURL.searchParams.append("q", query)
-			const body = await fetch(remoteURL.toString()).then(res => res.text())
+			const body = await fetch(remoteURL.toString(), {
+				headers: {
+					"Cookie": `server_nearest_store_v2={"StoreId":"d4408e0f-5268-42c2-ba76-2bc9732d4316","UserLat":"-46.0511","UserLng":"169.9467","StoreLat":"-41.334534","StoreLng":"174.772497","IsSuccess":true}; STORE_ID_V2=63cbb6c6-4a0b-448d-aa78-8046692a082c|False; Region=NI; AllowRestrictedItems=true; sxa_site=New World; fs-store-select-tooltip-closed=true`
+				}
+			}).then(res => res.text())
 			const dom = parse(body)
 
 			const results = dom.querySelectorAll(".fs-product-card").map(productCardElement => {
 				const productAnchorElement = productCardElement.querySelector(".fs-product-card__row").childNodes[1]
 				const productDetailsElement = productCardElement.querySelector(".js-product-card-footer")
 				const productImageElement = productCardElement.querySelector(".fs-product-card__product-image")
-				const data = JSON.parse(productDetailsElement.attributes["data-options"])
-				const capacity = productCardElement.querySelector(".u-p3")
+				let productOptions = productDetailsElement.rawAttrs.match(/^.*? data-options='(\{.*?),\s*"clubCardComponentStrings/ms)[1] + "}}"
+				productOptions = entities.decodeHTML5(productOptions)
+				const data = JSON.parse(productOptions)
+				let capacityText = productCardElement.querySelector(".u-p3").text.trim()
+				if (capacityText.includes("\n")) capacityText = ""
+				const imageURL = productImageElement.rawAttrs.match(/data-src-s="([^"]+)"/)[1]
+				const link = productAnchorElement.rawAttrs.match(/href="([^"]+)"/)[1]
 				return {
 					name: data.productName,
 					price: Number(data.ProductDetails.PricePerItem),
 					priceMode: data.ProductDetails.PriceMode,
-					capacity: capacity.text.trim(),
-					href: `https://www.ishopnewworld.co.nz${productAnchorElement.attributes.href}`,
-					image: productImageElement.attributes.style.match(/background(?:-image)?: url\('?([^')]+)'?\)/)[1]
+					capacity: capacityText,
+					href: `https://www.newworld.co.nz${link}`,
+					image: imageURL
 				}
 			})
 
